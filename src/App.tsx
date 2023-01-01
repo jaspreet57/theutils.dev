734ad6bd3d149717af8b7d3ff4415e6f626131e4
Jaspreet Singh <erjaspreet57@gmail.com>
2023-01-01 15:36:24 +0530

    refactor: initial cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import Logo from './assets/svgs/Logo'
-import './App.css';
+import Logo from './assets/svgs/Logo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{ width: '400px', height: '400px' }}>
-          <Logo />
-        </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '400px', height: '400px' }}>
+      <Logo />
     </div>
   );
 }
